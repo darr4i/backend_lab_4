18a4fcb68762d058f360eee4abf5a769a20d5b46
darr4i <doshirrak999@gmail.com>
2024-11-26 21:57:50 +0200

    added user table

--- app/models.py
+++ app/models.py
@@ -8,3 +8,18 @@ class Account(db.Model):
 
     def __repr__(self):
         return f'<Account {self.id}, Balance: {self.balance}>'
+from passlib.hash import pbkdf2_sha256
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    @staticmethod
+    def hash_password(password):
+        return pbkdf2_sha256.hash(password)
+
+    @staticmethod
+    def verify_password(password, hashed):
+        return pbkdf2_sha256.verify(password, hashed)
