b345fb152b1e88c58274c175287d83c22fe95069
darr4i <doshirrak999@gmail.com>
2024-11-26 22:09:10 +0200

    added error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app.config import Config  
+from flask_jwt_extended import JWTManager
+from app.config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -13,7 +14,24 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
+    jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return jsonify({"message": "Token has expired", "error": "token_expired"}), 401
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({"message": "Invalid token", "error": "invalid_token"}), 401
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({"message": "Token is missing", "error": "authorization_required"}), 401
+
     from app.routes.accounts import account_bp
     app.register_blueprint(account_bp)
+
+    from app.routes.auth import auth_bp
+    app.register_blueprint(auth_bp)
 
     return app
