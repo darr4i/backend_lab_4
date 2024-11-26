774d74c2578f2a4444cc1ffe3cb618a027cb7ee3
darr4i <doshirrak999@gmail.com>
2024-11-26 21:56:13 +0200

    generated key

--- app/config.py
+++ app/config.py
@@ -6,3 +6,4 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "747719d235345d4332bb73922d45577fbd833daf8f9841f48a50091998827376")
